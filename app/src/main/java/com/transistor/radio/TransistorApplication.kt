09b41cf1a2d267a249cb7249ef4505c0d3de65c9
tehrzky <61300808+tehrzky@users.noreply.github.com>
2026-08-13 00:09:07 +0800

    Override attachBaseContext in TransistorApplication
    Override attachBaseContext to install CrashHandler.

--- app/src/main/java/com/transistor/radio/TransistorApplication.kt
+++ app/src/main/java/com/transistor/radio/TransistorApplication.kt
@@ -1,12 +1,13 @@
 package com.transistor.radio
 
 import android.app.Application
+import android.content.Context
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class TransistorApplication : Application() {
-    override fun onCreate() {
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
         CrashHandler.install(this)
-        super.onCreate()
     }
 }
