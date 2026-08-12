85f9889f39999d6a7aae3cdd36de16a0425e7125
tehrzky <61300808+tehrzky@users.noreply.github.com>
2026-08-13 00:50:07 +0800

    Clean up imports in RootScreen.kt
    Removed unused imports from RootScreen.kt

--- app/src/main/java/com/transistor/radio/ui/components/RootScreen.kt
+++ app/src/main/java/com/transistor/radio/ui/components/RootScreen.kt
@@ -3,8 +3,6 @@ package com.transistor.radio.ui.components
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
