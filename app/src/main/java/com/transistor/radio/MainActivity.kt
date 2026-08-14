f9fcfe47fcb097a5fef03ce02ab1d54f3267ddb8
tehrzky <61300808+tehrzky@users.noreply.github.com>
2026-08-14 10:07:52 +0800

    Install CrashHandler in MainActivity onCreate

--- app/src/main/java/com/transistor/radio/MainActivity.kt
+++ app/src/main/java/com/transistor/radio/MainActivity.kt
@@ -33,6 +33,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        CrashHandler.install(this)
         setContent {
             TransistorRadioTheme {
                 Surface(
